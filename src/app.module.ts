3c2419ca265adc04b5571ac14631e09ff0d877b4
Abdul-Qadir-Baloch <abdulqadir19974@gmail.com>
2023-03-24 14:00:06 +0500

    auth-svc

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: null,
+      username: 'postgres',
+      password: null,
+      entities: ['dist/**/*.entity.{ts,js}'],
+      autoLoadEntities:true,
+      synchronize: true, // never true in production!
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
